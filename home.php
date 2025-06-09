9a42957d0d52902aa9837ed021cea5c1e623962a
unknown <Jesuslovestestimony@gmail.com>
2025-06-09 20:06:44 +0100

    A better look

--- home.php
+++ home.php
@@ -156,20 +156,14 @@ $total_new_inventory = isset($result_inventory[0]['cnt']) ? $result_inventory[0]
 <div class="wrapper">
     <!-- Sidebar Navigation (Refactored to match your pasted code) -->
     <nav id="sidebar" class="sidebar">
-        <div class="sidebar-content js-simplebar">
-            <a class="sidebar-brand" href="home.php">
-                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="20px" height="20px" viewBox="0 0 20 20">
-                    <path d="M19.4,4.1l-9-4C10.1,0,9.9,0,9.6,0.1l-9,4C0.2,4.2,0,4.6,0,5s0.2,0.8,0.6,0.9l9,4C9.7,10,9.9,10,10,10s0.3,0,0.4-0.1l9-4
-                      C19.8,5.8,20,5.4,20,5S19.8,4.2,19.4,4.1z"/>
-                    <path d="M10,15c-0.1,0-0.3,0-0.4-0.1l-9-4c-0.5-0.2-0.7-0.8-0.5-1.3c0.2-0.5,0.8-0.7,1.3-0.5l8.6,3.8l8.6-3.8c0.5-0.2,1.1,0,1.3,0.5
-                      c0.2,0.5,0,1.1-0.5,1.3l-9,4C10.3,15,10.1,15,10,15z"/>
-                    <path d="M10,20c-0.1,0-0.3,0-0.4-0.1l-9-4c-0.5-0.2-0.7-0.8-0.5-1.3c0.2-0.5,0.8-0.7,1.3-0.5l8.6,3.8l8.6-3.8c0.5-0.2,1.1,0,1.3,0.5
-                      c0.2,0.5,0,1.1-0.5,1.3l-9,4C10.3,20,10.1,20,10,20z"/>
-                </svg>
-                <span class="align-middle me-3">Vuvaa Lifestyle</span>
-            </a>
-            <ul class="sidebar-nav">
-                <li class="sidebar-header">Navigation</li>
+    <div class="sidebar-content js-simplebar">
+        <a class="sidebar-brand bg-dark text-white" href="home.php" style="display: flex; align-items: center; padding: 1rem; border-bottom: 1px solid #222;">
+            <!-- If you have a logo SVG or image, put it here -->
+            <!-- <img src="img/icon.png" alt="Logo" width="32" height="32" class="me-2"> -->
+            <span class="align-middle me-3" style="font-weight:600;">Vuvaa Lifestyle</span>
+        </a>
+        <ul class="sidebar-nav">
+            <li class="sidebar-header">Navigation</li>
             <li class="sidebar-item <?php echo ($_SERVER['PHP_SELF'] == '/home.php') ? 'active' : ''; ?>">
                 <a class="sidebar-link" href="home.php">
                     <i class="align-middle" data-lucide="home"></i>
@@ -206,6 +200,9 @@ $total_new_inventory = isset($result_inventory[0]['cnt']) ? $result_inventory[0]
                 'files' => 'file-text',
                 'file manager' => 'folder',
                 'department' => 'grid',
+                'items category' => 'box',
+                'staff' => 'users',
+                'inventory' => 'archive',
                 'tasks' => 'check-square',
                 'task' => 'check-square',
                 'projects' => 'folder',
@@ -652,4 +649,3 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 });
 </script>
-<!-- ...existing code... -->
